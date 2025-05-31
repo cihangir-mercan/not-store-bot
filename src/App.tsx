37490e3e8e70e9435cd649a3dbb182f1c5300917
cihangir-mercan <g5837441b>
2025-05-31 20:25:14 +0300

    Try full screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import { Routes, Route } from "react-router"
 import { StorePage } from "@pages/store-page"
 import { UserPage } from "@pages/user-page"
 import { LayoutWithBottomTabs } from "@components/layout-with-bottom-tabs"
+import { useEffect } from "react"
 
 export const App = () => {
+  const tgWebApp = window.Telegram.WebApp;
+
+  useEffect(() => {
+    tgWebApp.expand();
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<LayoutWithBottomTabs />}>
