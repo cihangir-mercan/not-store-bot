a2e4afa82b98a0aba1ab8c2f43db7b9cca0dba88
cihangir-mercan <g5837441b>
2025-05-31 20:35:09 +0300

    Try full screen

--- src/components/layout-with-bottom-tabs/index.tsx
+++ src/components/layout-with-bottom-tabs/index.tsx
@@ -2,16 +2,20 @@ import type { JSX } from "react"
 import { NavLink, Outlet } from "react-router"
 import styles from "./styles/index.module.scss"
 import clsx from "clsx"
+import { BOTTOM_TABBAR_HEIGHT } from "@components/layout-with-bottom-tabs/constants"
 
 const getTabClasses = (isActive: boolean): string =>
   clsx(styles.tab, isActive && styles.tabActive)
 
 export const LayoutWithBottomTabs = (): JSX.Element => {
-  // const tg = window.Telegram.WebApp;
-  // const height = 60 + tg.safeAreaInset.bottom;
+  const tgWebApp = window.Telegram.WebApp;
+  const paddingBottom = BOTTOM_TABBAR_HEIGHT + tgWebApp.safeAreaInset.bottom;
 
   return (
-    <div className={styles.appContainer}>
+    <div
+      className={styles.appContainer}
+      style={{paddingBottom: paddingBottom}}
+    >
       {/* this is where the active page will render */}
       <div className={styles.content}>
         <Outlet />
