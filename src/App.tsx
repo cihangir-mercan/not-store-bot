11b1abc481a76808aed33bcaa69fb94f22de4b2a
cihangir-mercan <g5837441b>
2025-06-02 00:24:29 +0300

    Try share

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.scss"
-import { Routes, Route } from "react-router"
+import { Routes, Route, useNavigate } from "react-router"
 import { LayoutWithBottomTabs } from "@components/layout-with-bottom-tabs"
 import { ProductPage } from "@pages/product-page"
+import { useEffect } from "react"
 
 export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+
+    const param = tg.initDataUnsafe.start_param;
+    if (param) {
+      void navigate(`/product/${param}`);
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<LayoutWithBottomTabs />} />
