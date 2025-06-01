f86d8560cb7ffb1478cd8a778948219dea1422ab
cihangir-mercan <g5837441b>
2025-06-01 15:14:12 +0300

    Fix scroll restoration

--- src/components/layout-with-bottom-tabs/index.tsx
+++ src/components/layout-with-bottom-tabs/index.tsx
@@ -1,20 +1,24 @@
-import type { JSX } from "react"
+import type { JSX} from "react";
+import { useRef } from "react"
 import { NavLink, Outlet } from "react-router"
 import styles from "./styles/index.module.scss"
 import clsx from "clsx"
 import { BOTTOM_TABBAR_HEIGHT } from "@components/layout-with-bottom-tabs/constants"
+import { useScrollRestoration } from "@components/layout-with-bottom-tabs/hooks/useScrollRestoration.tsx"
 
 const getTabClasses = (isActive: boolean): string =>
   clsx(styles.tab, isActive && styles.tabActive)
 
 export const LayoutWithBottomTabs = (): JSX.Element => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  useScrollRestoration(scrollRef); // pass correct selector
   const tgWebApp = window.Telegram.WebApp
   const bottomInset = tgWebApp.safeAreaInset.bottom
   const offset = BOTTOM_TABBAR_HEIGHT + bottomInset
 
   return (
     <div className={styles.appContainer} style={{ paddingBottom: offset }}>
-      <div className={styles.content}>
+      <div className={styles.content}  ref={scrollRef}>
         <Outlet />
       </div>
 
