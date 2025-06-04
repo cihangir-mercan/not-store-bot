4f50e2eba9498ca4fff2e4533ed1febcb0f7b7f8
cihangir-mercan <g5837441b>
2025-06-04 13:09:33 +0300

    Try bottom inset

--- src/components/layout-with-bottom-tabs/index.tsx
+++ src/components/layout-with-bottom-tabs/index.tsx
@@ -1,24 +1,33 @@
-import type { JSX } from "react"
+import { JSX, useEffect, useState } from "react"
 import { useLocation } from "react-router"
 import styles from "./styles/index.module.scss"
 import { BOTTOM_TABBAR_HEIGHT } from "@components/layout-with-bottom-tabs/constants"
 import { StorePage } from "@pages/store-page"
 import { UserPage } from "@pages/user-page"
 import { BottomTabBar } from "@components/bottom-tab-bar"
+import { useAppSelector } from "@app/hooks.ts"
+import { selectSearchInputFocused } from "@app/slices/uiSlice.ts"
 
 export const LayoutWithBottomTabs = (): JSX.Element => {
   const location = useLocation()
-  const tgWebApp = window.Telegram.WebApp
-  const bottomInset = tgWebApp.safeAreaInset.bottom
-  const offset = BOTTOM_TABBAR_HEIGHT + bottomInset
+  const [initialBottomInset, setInitialBottomInset] = useState(0);
+  const offset = BOTTOM_TABBAR_HEIGHT + initialBottomInset
 
   const isStore = location.pathname === "/"
   const isUser = location.pathname === "/user"
+  const keyboardVisible = useAppSelector(selectSearchInputFocused)
+
+  useEffect(() => {
+    const tgWebApp = window.Telegram.WebApp
+    const bottomInset = tgWebApp.safeAreaInset.bottom
+    setInitialBottomInset(bottomInset);
+  }, [])
 
   return (
     <div className={styles.appContainer} style={{ paddingBottom: offset }}>
       <div className={styles.content}>
-        <p>offset: {offset}</p>
+        <p>initialBottomInset: {initialBottomInset}</p>
+        <p>keyboardVisible: {keyboardVisible}</p>
         <div className={styles.tabContent} data-active={isStore}>
           <StorePage />
         </div>
