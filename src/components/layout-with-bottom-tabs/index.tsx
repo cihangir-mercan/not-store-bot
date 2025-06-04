43f979c0866d9469f8b1be0f7140086f794c0d23
cihangir-mercan <g5837441b>
2025-06-04 12:53:38 +0300

    Try bottom inset

--- src/components/layout-with-bottom-tabs/index.tsx
+++ src/components/layout-with-bottom-tabs/index.tsx
@@ -5,28 +5,20 @@ import { BOTTOM_TABBAR_HEIGHT } from "@components/layout-with-bottom-tabs/consta
 import { StorePage } from "@pages/store-page"
 import { UserPage } from "@pages/user-page"
 import { BottomTabBar } from "@components/bottom-tab-bar"
-import { useAppSelector } from "@app/hooks.ts"
-import { selectSearchInputFocused } from "@app/slices/uiSlice.ts"
 
 export const LayoutWithBottomTabs = (): JSX.Element => {
   const location = useLocation()
   const tgWebApp = window.Telegram.WebApp
   const bottomInset = tgWebApp.safeAreaInset.bottom
   const offset = BOTTOM_TABBAR_HEIGHT + bottomInset
-  const viewport = tgWebApp.viewportHeight
 
   const isStore = location.pathname === "/"
   const isUser = location.pathname === "/user"
-  const keyboardVisible = useAppSelector(selectSearchInputFocused)
 
   return (
-    <div className={styles.appContainer}
-         style={{ paddingBottom: offset }}
-    >
+    <div className={styles.appContainer} style={{ paddingBottom: offset }}>
       <div className={styles.content}>
-        <div>bottomInset: {bottomInset}</div>
-        <div>viewport: {viewport}</div>
-        <div>keyboardVisible: {keyboardVisible}</div>
+        <p>offset: {offset}</p>
         <div className={styles.tabContent} data-active={isStore}>
           <StorePage />
         </div>
